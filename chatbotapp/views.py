1018d7473da0c8ba12e14f5e0e3585170a2bf00a
Álvaro Vega Gómez <alvarvg@gmail.com>
2024-11-21 13:47:22 +0100

    first commit
    base chatbot working, gemini recieves prompt and replies

---
+++ chatbotapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import redirect, render
+from triprecommendator.settings import GENERATIVE_AI_KEY
+from chatbotapp.models import ChatMessage
+import google.generativeai as genai
+
+
+def send_message(request):
+    if request.method == "POST":
+        genai.configure(api_key=GENERATIVE_AI_KEY)
+        model = genai.GenerativeModel("gemini-1.5-flash")
+
+        user_message = request.POST.get("user_message")
+        bot_response = model.generate_content(user_message)
+
+        ChatMessage.objects.create(user_message=user_message, bot_response=bot_response)
+    return redirect("list_messages")
+
+
+def list_messages(request):
+    messages = ChatMessage.objects.all()
+    return render(request, "chatbot/list_messages.html", {"messages": messages})
