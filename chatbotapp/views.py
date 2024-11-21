8bef7ece7cdf631bdd2cbdcb876d07a2d8cbf103
Álvaro Vega Gómez <alvarvg@gmail.com>
2024-11-21 15:24:51 +0100

    styles, volumes, markdown
    added basic styling
    added volumes for fast reload in dev
    added markdown parsing for prompts

--- chatbotapp/views.py
+++ chatbotapp/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import redirect, render
 from triprecommendator.settings import GENERATIVE_AI_KEY
 from chatbotapp.models import ChatMessage
 import google.generativeai as genai
+import markdown
 
 
 def send_message(request):
+    md = markdown.Markdown(extensions=["extra"])
     if request.method == "POST":
         genai.configure(api_key=GENERATIVE_AI_KEY)
         model = genai.GenerativeModel("gemini-1.5-flash")
 
         user_message = request.POST.get("user_message")
-        bot_response = model.generate_content(user_message)
+        text_content = model.generate_content(user_message)
 
+        bot_response = md.convert(str(text_content.candidates[0].content.parts[0].text))
         ChatMessage.objects.create(user_message=user_message, bot_response=bot_response)
     return redirect("list_messages")
 
